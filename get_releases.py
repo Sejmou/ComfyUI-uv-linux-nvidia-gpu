dcb30329a50979e1d4ca29e36dc4dce50bf19d79
Sejmou <samo.kolter@gmail.com>
2026-02-10 22:21:41 +0100

    implement basic install script

--- get_releases.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-response = requests.get(
-    "https://api.github.com/repos/Comfy-Org/ComfyUI/releases",
-    headers={"Accept": "application/vnd.github+json"},
-)
-releases = response.json()
-for release in releases:
-    print(release["tag_name"], release["published_at"])
--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import tomllib
 import typer
 from pathlib import Path
 from typing import Optional
@@ -15,7 +16,7 @@ def main(
         Path("~", "comfyui").expanduser(),  # ~/comfyui default
         "--folder",
         "-f",
-        prompt=True,  # ALWAYS prompt (editable default)
+        prompt="Enter the path to the folder to install ComfyUI in (will be created if it doesn't exist)",
         dir_okay=True,
         file_okay=False,
         exists=False,
@@ -32,40 +33,59 @@ def main(
 
     # check if git repo exists
     if not (folder / ".git").exists():
-        clone_repo(folder)
-    else:
-        # get latest commit hash and date
-        commit_hash = (
-            subprocess.run(["git", "rev-parse", "HEAD"], capture_output=True)
-            .stdout.decode("utf-8")
-            .strip()
-        )
-        commit_date = (
-            subprocess.run(["git", "log", "-1", "--format=%ad"], capture_output=True)
-            .stdout.decode("utf-8")
-            .strip()
-        )
-        typer.echo(f"Latest commit: {commit_hash} on {commit_date}")
-        # ask user if they want to pull latest changes
-        if typer.confirm("Do you want to pull latest changes?"):
-            pull_latest(folder)
+        clone_repo()
 
-    create_or_update_pyproject(folder)
+    update()
 
 
-def clone_repo(folder: Path):
+def clone_repo():
     typer.echo(f"Cloning repo {REPO_URL}")
-    subprocess.run(["git", "clone", REPO_URL])
+    subprocess.run(["git", "clone", REPO_URL, "."])
 
 
-def pull_latest(folder: Path):
-    typer.echo(f"Pulling latest changes from {REPO_URL}")
-    subprocess.run(["git", "pull", REPO_URL])
-
-
-def create_or_update_pyproject(folder: Path):
+def update():
     typer.echo(f"Creating or updating pyproject.toml for uv")
+    # extract version from current pyproject.toml
+    toml_path = Path(os.getcwd(), "pyproject.toml")
+
+    data = tomllib.loads(toml_path.read_text(encoding="utf-8"))
+    version = data["project"]["version"]
+
+    typer.echo(f"Current version: {version}")
+    # remove the repo's pyproject.toml; conflicts with pyproject.toml used by uv!
+    toml_path.unlink()
+
+    subprocess.run(["uv", "init", "--python", "3.13"])
+
     subprocess.run(["uv", "add", "-r", "requirements.txt"])
+
+    install_manager = typer.confirm(
+        "Should ComfyUI manager be installed too? - more details: https://github.com/Comfy-Org/ComfyUI-Manager/tree/manager-v4",
+        default=True,
+    )
+    if install_manager:
+        subprocess.run(["uv", "add", "-r", "manager_requirements.txt"])
+
+    # add the CUDA GPU config to pyproject.toml
+    cuda_gpu_config = """[[tool.uv.index]]
+name = "pytorch-cu130"
+url = "https://download.pytorch.org/whl/cu130"
+explicit = true
+[tool.uv.sources]
+torch = [
+  { index = "pytorch-cu130", marker = "sys_platform == 'linux' or sys_platform == 'win32'" },
+]
+torchvision = [
+  { index = "pytorch-cu130", marker = "sys_platform == 'linux' or sys_platform == 'win32'" },
+]
+torchaudio = [
+  { index = "pytorch-cu130", marker = "sys_platform == 'linux' or sys_platform == 'win32'" },
+]"""
+    with open("pyproject.toml", "a") as f:
+        f.write(cuda_gpu_config)
+
+    # run uv sync to install the dependencies (this time from proper package index)
+    subprocess.run(["uv", "sync"])
 
 
 if __name__ == "__main__":
